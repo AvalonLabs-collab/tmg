81c5e5ab7af2646ac104a5bdef83cdd166fc9e96
avalon <tochukwuemmanuel171@gmail.com>
2026-02-07 14:19:49 +0100

    Fix: Add TrustProxies middleware and HTTPS configuration for Railway

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Middleware\EnsureAuthenticated;
+use App\Http\Middleware\TrustProxies;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -12,6 +13,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->use([
+            TrustProxies::class,
+        ]);
+        
         $middleware->alias([
             'ensure.auth' => EnsureAuthenticated::class,
         ]);
