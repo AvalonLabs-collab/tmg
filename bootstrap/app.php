7206d67fbda198691957da8a77c85df006aa862e
avalon <tochukwuemmanuel171@gmail.com>
2026-02-10 13:36:26 +0100

    great work

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,7 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->use([
             TrustProxies::class,
         ]);
-        
+
         $middleware->alias([
             'ensure.auth' => EnsureAuthenticated::class,
         ]);
