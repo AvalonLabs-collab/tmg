782588e90a9aed2d08d939898169acebf4218796
avalon <tochukwuemmanuel171@gmail.com>
2026-02-13 22:11:02 +0100

    prepared for client 35

--- resources/views/livewire/vehicle-gallary.blade.php
+++ resources/views/livewire/vehicle-gallary.blade.php
@@ -3,26 +3,34 @@
              <div class="col-xs-10">
                  <ul class="b-detail__main-info-images-big bxslider enable-bx-slider" data-pager-custom="#bx-pager"
                      data-mode="horizontal" data-pager-slide="true" data-mode-pager="vertical" data-pager-qty="5">
-                     {{-- @if ($vehicleImages)
-                         @foreach ($vehicleImages as $image)
+                             @forelse ($vehicleImages as $image)
                              <li class="s-relative">
                                  <a data-toggle="modal" data-target="#myModal" href="#"
                                      class="b-items__cars-one-img-video"><span class="fa fa-film"></span>VIDEO</a>
                                  <img class="img-responsive center-block" src="{{ asset($image) }}"
                                      alt="nissan" />
                              </li>
-                         @endforeach
-                     @endif --}}
+                    @empty
+    <p>No images available.</p>
+@endforelse
+
+
+
                  </ul>
              </div>
              <div class="col-xs-2 pagerSlider pagerVertical">
                  <div class="b-detail__main-info-images-small" id="bx-pager">
+                    @forelse ( $vehicleImages as $item )
+                    <a data-slide-index="{{ $loop->index }}" href="#" class="b-detail__main-info-images-small-one">
+                        <img class="img-responsive center-block" src="{{ asset($item) }}" alt="nissan" />
+                    </a>
+                    @empty
+                    <p>no images found</p>
+
+                    @endforelse
                      {{-- @if ($vehicleImages)
-                         @foreach ($vehicleImages as $item)
-                             <a href="#" data-slide-index="{{ $loop->index }}"
-                                 class="b-detail__main-info-images-small-one">
-                                 <img class="img-responsive" src="{{ asset($item) }}" alt="nissan" />
-                             </a>
+                         @foreach ()
+
                          @endforeach
                      @endif --}}
                  </div>
