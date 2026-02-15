5971d60127477da2562974a1dbedc7ee53aa4b30
avalon <tochukwuemmanuel171@gmail.com>
2026-02-15 10:48:07 +0100

    prepared for client 60

--- app/Livewire/Recomendation.php
+++ app/Livewire/Recomendation.php
@@ -4,6 +4,10 @@ namespace App\Livewire;
 
 use App\Reccomendation as Reccomend;
 use Livewire\Component;
+use App\Models\Vehicle;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 use function Illuminate\Log\log;
 
@@ -11,14 +15,59 @@ class Recomendation extends Component
 {
     public $reccomendation;
 
+        public function recommend()
+    {
+        [$column, $identifier] = $this->resolveViewerIdentity();
+
+        $viewedVehicleIds = DB::table('vehicle_views')
+            ->where($column, $identifier)
+            ->latest()
+            ->pluck('vehicle_id');
+
+        if ($viewedVehicleIds->isEmpty()) {
+            return $this->fallbackRecommendations();
+        }
+
+        $baseVehicles = Vehicle::whereIn('id', $viewedVehicleIds)->get();
+        Log::info($viewedVehicleIds);
+
+        if ($baseVehicles->isEmpty()) {
+            return $this->fallbackRecommendations();
+        }
+
+        $makes = $baseVehicles->pluck('make')->unique();
+        $models = $baseVehicles->pluck('model')->unique();
+
+        return Vehicle::query()
+            ->whereNotIn('id', $viewedVehicleIds)
+            ->where(function ($query) use ($makes, $models) {
+                $query->whereIn('model', $models)
+                    ->orWhereIn('make', $makes);
+            })
+            ->get()
+            ->toArray();
+    }
+
+    protected function resolveViewerIdentity(): array
+    {
+        if (Auth::check()) {
+            return ['user_id', Auth::id()];
+        }
+
+        return ['guest_id', session()->getId()];
+    }
+
+    protected function fallbackRecommendations()
+    {
+        return Vehicle::latest()
+            ->limit(40)
+            ->get()
+            ->toArray();
+    }
+
     public function createList()
     {
-        try {
-            $rec = new Reccomend;
-            $this->reccomendation = $rec->recommend();
-        } catch (\Exception $e) {
-            log('Error in Reccomendation mount: '.$e->getMessage());
-        }
+        $this->reccomendation = $this->recommend();
     }
 
     public function render()
