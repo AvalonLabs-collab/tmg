58ec0eca7aefe90b59d9ee8168ffc56c07dcd40e
avalon <tochukwuemmanuel171@gmail.com>
2026-02-15 08:04:00 +0100

    prepared for client 41

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -26,7 +26,7 @@
              <div class="col-xs-10">
                  <ul class="b-detail__main-info-images-big bxslider enable-bx-slider" data-pager-custom="#bx-pager"
                      data-mode="horizontal" data-pager-slide="true" data-mode-pager="vertical" data-pager-qty="5">
-                             @forelse ($vehicle->images as $image)
+                             {{-- @forelse ($vehicle->images as $image)
                              <li class="s-relative">
                                  <a data-toggle="modal" data-target="#myModal" href="#"
                                      class="b-items__cars-one-img-video"><span class="fa fa-film"></span>VIDEO</a>
@@ -35,19 +35,19 @@
                              </li>
                     @empty
     <p>No images available.</p>
-@endforelse
+@endforelse --}}
                  </ul>
              </div>
              <div class="col-xs-2 pagerSlider pagerVertical">
                  <div class="b-detail__main-info-images-small" id="bx-pager">
-                    @forelse ( $vehicle->images as $item )
+                    {{-- @forelse ( $vehicle->images as $item )
                     <a data-slide-index="{{ $loop->index }}" href="#" class="b-detail__main-info-images-small-one">
                         <img class="img-responsive center-block" src="{{ asset('storage/'.$item) }}" alt="nissan" />
                     </a>
                     @empty
                     <p>no images found</p>
 
-                    @endforelse
+                    @endforelse --}}
                  </div>
              </div>
          </div>
