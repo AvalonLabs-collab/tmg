bcf66323229bcdfb653090361d9087853cf1872e
avalon <tochukwuemmanuel171@gmail.com>
2026-02-15 09:19:33 +0100

    prepared for client 48

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -26,33 +26,33 @@
              <div class="col-xs-10">
                  <ul class="b-detail__main-info-images-big bxslider enable-bx-slider" data-pager-custom="#bx-pager"
                      data-mode="horizontal" data-pager-slide="true" data-mode-pager="vertical" data-pager-qty="5">
-                         @if (isset($vehicle->images && $vehicle->images !== false))
-                                   @forelse ($vehicle->images as $image)
+                         {{-- @if (isset($vehicle->images && $vehicle->images !== false))
+                                   @forelse ($vehicle->images as $image) --}}
                              <li class="s-relative">
                                  <a data-toggle="modal" data-target="#myModal" href="#"
                                      class="b-items__cars-one-img-video"><span class="fa fa-film"></span>VIDEO</a>
-                                 <img class="img-responsive center-block" src="{{ asset($image) }}"
+                                 <img class="img-responsive center-block" src="{{ asset('assets/media/237x202/mersList.jpg') }}"
                                      alt="nissan" />
                              </li>
-                    @empty
+                    {{-- @empty
     <p>No images available.</p>
 @endforelse
-                         @endif
+                         @endif --}}
 
                  </ul>
              </div>
              <div class="col-xs-2 pagerSlider pagerVertical">
                  <div class="b-detail__main-info-images-small" id="bx-pager">
-                        @if (isset($vehicle->images) && $vehicle->images !== false)
-                    @forelse ( $vehicle->images as $item )
+                        {{-- @if (isset($vehicle->images) && $vehicle->images !== false)
+                    @forelse ( $vehicle->images as $item ) --}}
                     <a data-slide-index="{{ $loop->index }}" href="#" class="b-detail__main-info-images-small-one">
-                        <img class="img-responsive center-block" src="{{ asset($item) }}" alt="nissan" />
+                        <img class="img-responsive center-block" src="{{ asset('assets/media/237x202/mersList.jpg') }}" alt="nissan" />
                     </a>
-                    @empty
+                    {{-- @empty
                     <p>no images found</p>
 
                     @endforelse
-                      @endif
+                      @endif --}}
                  </div>
              </div>
          </div>
@@ -101,7 +101,7 @@
                                         <p>5</p>
                                     </div>
                                     <div class="b-detail__main-info-characteristics-one-bottom">
-                                        Passangers
+                                        PassangerJJs
                                     </div>
                                 </div>
                                 <div class="b-detail__main-info-characteristics-one">
